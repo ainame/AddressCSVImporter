e14cee3b7331a931c2ed9c1bb58d9d0778e68297
ainame <s.namai.09@gmail.com>
2016-09-27 22:50:12 +0900

    Initial Commit

---
+++ AddressCSVImporter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AddressCSVImporter
+//
+//  Created by satoshi.namai on 2016/09/27.
+//  Copyright © 2016年 生井 智司. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AddressCSVImporter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AddressCSVImporter
+//
+//  Created by satoshi.namai on 2016/09/27.
+//  Copyright © 2016年 生井 智司. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
