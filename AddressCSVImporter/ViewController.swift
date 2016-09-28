7a55ba3123ae778090b9010ad56ce2fcba7b43b7
ainame <s.namai.09@gmail.com>
2016-09-28 10:06:29 +0900

    Impl importer

--- AddressCSVImporter/ViewController.swift
+++ AddressCSVImporter/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import Cocoa
+import RealmSwift
+import RealmConverter
 
 class ViewController: NSViewController {
 
@@ -14,9 +16,21 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let filePaths = [
+            NSBundle.mainBundle().pathForResource("normalize", ofType: "csv")!
+        ]
+        let destinationRealmPath = "/Users/satoshi.namai/"
+
+        // Analyze the files and produce a Realm-compatible schema
+        let generator =  ImportSchemaGenerator(files: filePaths)
+        let schema = try! generator.generate()
+
+        // Use the schema and files to create the Realm file, and import the data
+        let dataImporter = CSVDataImporter(files: filePaths)
+        try! dataImporter.importToPath(destinationRealmPath, schema: schema)
     }
 
-    override var representedObject: Any? {
+    override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
         }
